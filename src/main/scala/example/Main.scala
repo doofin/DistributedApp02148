ef8f3893d33cf776769991ff956b7136a848ca49
doofin <8177dph@gmail.com>
2022-01-02 19:52:12 +0100

    init

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -6,9 +6,7 @@ import org.jspace.Space
 
 object Main {
   def main(args: Array[String]) = {
-    import org.jspace.FormalField
-    import org.jspace.SequentialSpace
-    import org.jspace.Space
+
     val inbox = new SequentialSpace
 
     inbox.put("Hello World!")
