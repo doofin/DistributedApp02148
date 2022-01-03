4e096f74954f7b1b615129476a802dede435d70a
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2022-01-03 23:56:29 +0100

    Added ScalaSpaces – a nicer API for Spaces

--- build.sbt
+++ build.sbt
@@ -10,8 +10,7 @@ lazy val root = (project in file("."))
     name := "DistributedApp",
     libraryDependencies ++= Seq(
       "com.github.pSpaces" % "jSpace" % "9ff32b60f1",
-      "com.lihaoyi" %% "pprint" % "0.5.9"
+      "com.lihaoyi" %% "pprint" % "0.5.9",
     )
-//    libraryDependencies += scalaTest % Test
   )
 
--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -1,21 +1,8 @@
 package example
 
-import org.jspace.FormalField
-import org.jspace.SequentialSpace
-import org.jspace.Space
-
 object Main {
   def main(args: Array[String]) = {
-    //lec1_fridge.run
     lec2_philosopher.run
   }
-
-  def test1 = {
-    val inbox = new SequentialSpace
-
-    inbox.put("Hello World!")
-    val tuple = inbox.get(new FormalField(classOf[String]))
-    System.out.println(tuple(0))
-
   }
 }
